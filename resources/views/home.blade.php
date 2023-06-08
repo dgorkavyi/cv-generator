bdb0333bcbc9650c6bc75c145fa259ed733e659f
dgorkavyi <dima.net.dd@gmail.com>
2023-06-08 15:45:39 +0300

    feature/add home page and layoute

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'CV Generator')
+@section('content')
+    <h1>HELLO</h1>
+@endsection('content')
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+<body>
+    <div class="container mx-auto">
+        @yield('content')
+    </div>
+</body>
